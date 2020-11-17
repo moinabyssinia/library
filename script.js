d44b2532ccf0b23fb751f523eabc620561a6c85a
moinabyssinia <michaelg.tadesse@gmail.com>
2020-11-17 06:42:12 -0500

    part 3 done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let books = [];
+let myLibrary = [];
 
 //constructor for book objects
 function Book(name, author, pages){
@@ -10,7 +10,30 @@ function Book(name, author, pages){
 //function to add book objects to the array
 const addBooks = function(name, author, pages){
     let newBook = new Book(name, author, pages);
-    books.push(newBook);
+    myLibrary.push(newBook);
 }
 
+// manually add to library
+addBooks("grant", "ron chernow", 289)
+addBooks("guns, germs, and steel", "jared diamond", 332)
+addBooks("genghis khan and the making of the moder world", "jack wheatherford", 430)
+addBooks("leningrad", "anna reid", 434)
+addBooks("embers of war", "fredrik logvall", 120)
+addBooks("rites of spring", "modris eksteins", 540)
+addBooks("the history of the ancient world", "susan bauer", 652)
+
 //function to loop through array and display
+const printBooks = function(){
+    myLibrary.forEach(function(element){
+        // create new p element
+        const newp = document.createElement("p");
+        // give it some content
+        const newContent = document.createTextNode(element.name);
+        // add the text node to the newly created p
+        newp.appendChild(newContent);
+        // add newly created element and its content into DOM
+        document.getElementById("myBooks").appendChild(newp)
+    })
+}
+
+printBooks()
